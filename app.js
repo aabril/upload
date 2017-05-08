4287d11a758a534e7182f77297eee25d3b45be84
Albert Abril <albert.abril@gmail.com>
2017-05-08 18:24:23 +0200

    enable list files

--- app.js
+++ app.js
@@ -3,15 +3,14 @@ const fileuploadMiddleware = require('express-fileupload')
 
 const indexCtrl = require('./controllers/indexCtrl')
 const uploadCtrl = require('./controllers/uploadCtrl')
-//const listCtrl = require('./controllers/listCtrl')
+const listCtrl = require('./controllers/listCtrl')
 
 const app = express();
 app.use(fileuploadMiddleware())
 
 app.get('/', indexCtrl)
 app.post('/upload', uploadCtrl)
-//app.get('/list', listCtrl)
-
+app.get('/list', listCtrl)
 
 const port = process.env.PORT || '3000'
 app.listen(port, function () {
