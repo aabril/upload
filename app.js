1d72523baba85041c6b058aacb31216d8003a88f
Albert Abril <albert.abril@gmail.com>
2017-05-08 17:18:52 +0200

    test why is it failing on dokku 2

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const fileuploadMiddleware = require('express-fileupload')
 
+const indexCtrl = require('./controllers/indexCtrl')
 const uploadCtrl = require('./controllers/uploadCtrl')
-//const indexCtrl = require('./controllers/indexCtrl')
 //const listCtrl = require('./controllers/listCtrl')
 
 const app = express();
 app.use(fileuploadMiddleware())
 
 app.get('/', indexCtrl)
+app.post('/upload', uploadCtrl)
 //app.get('/list', listCtrl)
-//app.post('/upload', uploadCtrl)
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
