ccb88961cde73eaacd7543a0ca822e1ff8568efa
Albert Abril <albert.abril@gmail.com>
2017-05-08 18:18:21 +0200

    test why is it failing on dokku 7

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.get('/', indexCtrl)
 app.post('/upload', uploadCtrl)
 //app.get('/list', listCtrl)
 
-app.listen(3001, function () {
-  console.log('Example app listening on port 3001!')
+app.listen(80, function () {
+  console.log('Example app listening on port 80!')
 })
