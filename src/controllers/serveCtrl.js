030c1e14beed5f7678770938d19d09389811dfad
Albert Abril <albert.abril@gmail.com>
2017-05-09 11:49:20 +0200

    serveCtrl has now handleVideo and handleImage or others functions

--- src/controllers/serveCtrl.js
+++ src/controllers/serveCtrl.js
@@ -4,33 +4,13 @@ const sharp = require('sharp')
 
 const DEFAULTS_IMG_MAX_WIDTH = 1200;
 
-const serveCtrl = (req, res) => {
-  const filename = req.params.filename
-  const filePath = config.PATH_DIR + '/' + filename
-  const fileMimetype = mime.lookup(filePath)
-  let width, height;
-
-  const imageMimetypes = [
-    'image/png',
-    'image/jpeg',
-    'image/webp',
-    'image/gif',
-    'image/svg+xml',
-    'image/tiff'
-  ]
-
-  if(imageMimetypes.includes(fileMimetype)){
-    const image = sharp(filePath);
-  }else{
-    return res.sendFile(filePath);
-  }
-  
+function handleImage(req, res, filePath, fileMimetype){
   const image = sharp(filePath);
+  let width, height;
 
   image
    .metadata()
    .then((metadata) => {
-      console.log(metadata)
       if(req.params.width && req.params.height){
         width = parseInt(req.params.width)
         height = parseInt(req.params.height)
@@ -51,15 +31,41 @@ const serveCtrl = (req, res) => {
         return res.send(buf);
       })
   })
+}
 
-  // sharp(filePath)
-  //   .resize(width, height)
-  //   .toBuffer((err, buf) => {
-  //     if(err) return console.log(err)
-  //     res.contentType(fileMimetype)
-  //     return res.send(buf);
-  //   })
+function handleVideo(req, res, filePath, fileMimetype) {
+    return res.sendFile(filePath)
+}
 
+const serveCtrl = (req, res) => {
+  const filename = req.params.filename
+  const filePath = config.PATH_DIR + '/' + filename
+  const fileMimetype = mime.lookup(filePath)
+
+  const imageMimetypes = [
+    'image/png',
+    'image/jpeg',
+    'image/webp',
+    'image/gif',
+    'image/svg+xml',
+    'image/tiff'
+  ]
+
+  const videoMimetypes = [
+    'video/quicktime',
+    'video/webm',
+    'video/mp4',
+    'video/x-msvideo',
+    'video/x-ms-wmv'
+  ]
+
+  if( imageMimetypes.includes(fileMimetype) ) {
+    handleImage(req, res, filePath, fileMimetype)
+  }else if( videoMimetypes.includes(fileMimetype) ) {
+    handleVideo(req, res, filePath, fileMimetype)
+  }else{
+    return res.sendFile(filePath)
+  }
 }
 
 module.exports = serveCtrl
