a2e6966549cd46f06a8f7c13ec7623349e37e02a
Albert Abril <albert.abril@gmail.com>
2017-05-09 02:25:30 +0200

    filtering by mime type

--- src/controllers/serveCtrl.js
+++ src/controllers/serveCtrl.js
@@ -6,22 +6,53 @@ const serveCtrl = (req, res) => {
   const filename = req.params.filename
   const filePath = config.PATH_DIR + '/' + filename
   const fileMimetype = mime.lookup(filePath)
+  let width, height;
 
-  if(req.params.width && req.params.height){
-    const width = parseInt(req.params.width)
-    const height = parseInt(req.params.height)
+  const imageMimetypes = [
+    'image/png',
+    'image/jpeg',
+    'image/webp',
+    'image/gif',
+    'image/svg+xml',
+    'image/tiff'
+  ];
 
-    sharp(filePath)
+  if(imageMimetypes.includes(fileMimetype)){
+    const image = sharp(filePath);
+  }else{
+    return res.sendFile(filePath);
+  }
+  
+  const image = sharp(filePath);
+
+  image
+   .metadata()
+   .then((metadata) => {
+      console.log(metadata)
+      if(req.params.width && req.params.height){
+        width = parseInt(req.params.width)
+        height = parseInt(req.params.height)
+      }else{
+        width = 400;
+      }
+   })
+  .then((metadata) => {
+    image
       .resize(width, height)
       .toBuffer((err, buf) => {
         if(err) return console.log(err)
         res.contentType(fileMimetype)
         return res.send(buf);
       })
+  })
 
-  }else{
-    return res.sendFile(filePath)
-  }
+  // sharp(filePath)
+  //   .resize(width, height)
+  //   .toBuffer((err, buf) => {
+  //     if(err) return console.log(err)
+  //     res.contentType(fileMimetype)
+  //     return res.send(buf);
+  //   })
 
 }
 
